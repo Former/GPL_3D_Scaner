7f080d99b522c14ba6f879bf0adabe49b9c75e03
Alexei <alexeibv@narod.ru>
2012-01-15 22:49:35 +0400

    Init

---
+++ main.cpp
@@ -0,0 +1,48 @@
+#include "main.h"
+
+IMPLEMENT_APP(MainApp);
+
+bool MainApp::OnInit()
+{
+	SetTopWindow( new MainDialog( NULL ) );
+	GetTopWindow()->Show();
+	
+	wxInitAllImageHandlers();
+	
+	// true = enter the main loop
+	return true;
+}
+
+MainDialog::MainDialog(wxWindow *parent) : MainDialogBase( parent )
+{
+	m_Device = new Device();
+	m_Device->openDevice(640, 480, -1);
+}
+
+MainDialog::~MainDialog()
+{
+	delete m_Device;	
+}
+
+void MainDialog::OnCloseDialog(wxCloseEvent& event)
+{
+	Destroy();
+}
+
+void MainDialog::OnOKClick(wxCommandEvent& event)
+{
+	char* buffer = 0;
+	m_Device->getFrame(&buffer);
+	wxImage wximg(640, 480);
+	wximg.SetData((unsigned char*)buffer);
+	
+	wxPaintDC dc(m_DrawWindow);
+	
+	dc.DrawBitmap(wximg, wxPoint(0, 0));
+}
+
+void MainDialog::OnCancelClick(wxCommandEvent& event)
+{
+	Destroy();
+}
+
---
+++ main.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <wx/wx.h>
+#include "gui.h"
+#include "device.h"
+
+class MainApp : public wxApp
+{
+	public:
+		virtual bool OnInit();
+};
+
+DECLARE_APP(MainApp)
+
+class MainDialog : public MainDialogBase
+{
+	public:
+		MainDialog( wxWindow *parent );
+		virtual ~MainDialog();
+		
+	protected:
+		virtual void OnCloseDialog( wxCloseEvent& event );
+		virtual void OnOKClick( wxCommandEvent& event );
+		virtual void OnCancelClick( wxCommandEvent& event );
+		
+		Device* m_Device;
+};
